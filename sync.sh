23d1d631d78855563da38ed600a86c5d2941f6fb
JackGrence <green1181772@gmail.com>
2018-01-31 22:02:42 -0800

    Move all config files to configs folder & Remove sync.sh

--- sync.sh
+++
@@ -1,6 +0,0 @@
-read -p "Sync UltiSnips?" yn
-case $yn in
-    [Yy]* ) cp -r ./.vim/UltiSnips/. ~/.vim/UltiSnips/. && echo "success" || echo "fail";;
-    [Nn]* ) exit;;
-    * ) echo "Please answer yes or no.";;
-esac
