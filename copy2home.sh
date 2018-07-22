68b9f3b11eefac2848395c89d4841b173092295e
JackGrence <green1181772@gmail.com>
2018-07-22 13:52:35 +0800

    Add ohmytmux and powerline-shell

--- copy2home.sh
+++ copy2home.sh
@@ -1,2 +1,8 @@
+pushd ~
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+popd
+sudo pip install powerline-shell
 cp -a configs/. ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
