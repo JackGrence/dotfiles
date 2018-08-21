f1bdc76e1eea895eb64c68eb16877f067901171e
JackGrence <green1181772@gmail.com>
2018-08-21 15:49:13 +0800

    Install python-pip, python-dev and build-essential

--- copy2home.sh
+++ copy2home.sh
@@ -3,6 +3,7 @@ git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
 popd
+sudo apt-get install python-pip python-dev build-essential
 sudo pip install powerline-shell
 cp -a configs/. ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
