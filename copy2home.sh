718d4a5bfd412c63258316ba74ca1d11530a141a
JackGrence <green1181772@gmail.com>
2018-12-15 02:58:24 -0800

    Add vim stuff

--- copy2home.sh
+++ copy2home.sh
@@ -1,9 +1,27 @@
+#!/bin/bash
+
+# install needed package
+sudo apt-get install python-pip python-dev build-essential tmux automake pkg-config
+sudo pip install powerline-shell
 pushd ~
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
 popd
-sudo apt-get install python-pip python-dev build-essential
-sudo pip install powerline-shell
+
+# copy my config
 cp -a configs/. ~/
+
+# install vim stuff
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c 'PluginInstall' -c 'qa!'
+
+# install ctag
+pushd /tmp
+git clone https://github.com/universal-ctags/ctags.git
+cd ctags
+./autogen.sh
+./configure
+make
+sudo make install
+popd
