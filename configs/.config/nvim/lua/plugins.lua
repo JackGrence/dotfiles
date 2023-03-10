81c8d90abcb59ac9af8f7923372ea3bb0a94548a
JackGrence <green1181772@gmail.com>
2023-03-10 11:35:04 +0800

    Add gitsigns

--- configs/.config/nvim/lua/plugins.lua
+++ configs/.config/nvim/lua/plugins.lua
@@ -131,4 +131,12 @@ return require('packer').startup(function(use)
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
+  use {
+    'lewis6991/gitsigns.nvim',
+    -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+    config = function ()
+      require('gitsigns').setup()
+    end
+  }
+
 end)
