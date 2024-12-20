b720a883ca0d716557a394396dda29feb3e13f40
JackGrence <green1181772@gmail.com>
2024-12-20 11:40:11 +0800

    Hide right prompt

--- fish/functions/hideps.fish
+++ fish/functions/hideps.fish
@@ -2,8 +2,12 @@ function hideps
   function fish_prompt
     echo '$ '
   end
+  function fish_right_prompt
+    echo ''
+  end
 end
 
 function unhideps
   source ~/.local/share/omf/themes/bobthefish/functions/fish_prompt.fish
+  source ~/.local/share/omf/themes/bobthefish/functions/fish_right_prompt.fish
 end
