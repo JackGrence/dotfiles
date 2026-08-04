11a01aa886da007f8b91e3aa118046ccf756a2d2
JackGrence <green1181772@gmail.com>
2026-08-04 11:34:37 +0800

    Add default lsp and disable default formating

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,6 +18,11 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- LAXYEXTRAS
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
     -- import/override with your plugins
     { import = "plugins" },
   },
---
+++ nvim/lua/plugins/formatting.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			-- This turns off formatting on save globally at startup
+			format = {
+				format_on_save = false,
+			},
+		},
+	},
+}
