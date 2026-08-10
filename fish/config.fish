1c238a5274b6620dd04a49edad464f59d38ac6bc
JackGrence <green1181772@gmail.com>
2026-08-10 11:53:26 +0800

    Fix non-exist path and cd to $HOME

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,8 @@ if status is-interactive
     fish_config theme choose "Dracula Official"
 
     if set -q ZELLIJ_CONTAINER_TARGET
-        set -l target_dir (docker exec coding-agent-$ZELLIJ_CONTAINER_TARGET cat /tmp/zellij_current_pane_dir.txt 2>/dev/null; or echo "/tmp")
+        set -l target_dir (docker exec -u ubuntu coding-agent-$ZELLIJ_CONTAINER_TARGET \
+          bash -c 'DIR="`cat /tmp/zellij_current_pane_dir.txt 2>/dev/null`"; [ -d "$DIR" ] && echo $DIR || echo $HOME')
         # If a Zellij layout set a container target, bypass the host and jump into Docker
         exec docker exec -it -e COLUMNS=(tput cols) -e LINES=(tput lines) \
             -e TERM=$TERM -w "$target_dir" -u ubuntu \
