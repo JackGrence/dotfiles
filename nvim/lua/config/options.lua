f831c2fe7174d47248e975f5743a93ae3f93671c
JackGrence <green1181772@gmail.com>
2026-08-31 14:53:20 +0800

    Add osc52

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,3 +4,27 @@
 
 vim.opt.wrap = true
 vim.opt.linebreak = true
+
+-- Enable the system clipboard register mapping
+vim.opt.clipboard = "unnamedplus"
+
+-- Fallback function to read Neovim's internal buffer for local pastes
+local function local_paste()
+	return {
+		vim.fn.split(vim.fn.getreg(""), "\n"),
+		vim.fn.getregtype(""),
+	}
+end
+
+-- Define the custom OSC 52 Clipboard provider
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = local_paste,
+		["*"] = local_paste,
+	},
+}
